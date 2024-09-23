909d274c5680c0676ad0f5b0381dd9633f365f0d
alex-cornwell <ac336@hawaii.edu>
2024-09-22 22:31:21 -1000

    E26

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,1 +1,30 @@
 "use strict";
+function forSum(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+function whileSum(numbers) {
+    let sum = 0;
+    let i = 0;
+    while (i < numbers.length) {
+        sum += numbers[i];
+        i++;
+    }
+    return sum;
+}
+function recursionSum(numbers) {
+    if (numbers.length === 0) {
+        return 0;
+    }
+    return numbers[0] + recursionSum(numbers.slice(1));
+}
+function functionSum(numbers) {
+    return numbers.reduce((acc, current) => acc + current, 0);
+}
+console.log('sumFor: ', forSum([1, 2, 3, 4]));
+console.log('sumWhile: ', whileSum([1, 2, 3, 4]));
+console.log('sumRecursion: ', recursionSum([1, 2, 3, 4]));
+console.log('sumTheFunctionalWay: ', functionSum([1, 2, 3, 4]));
